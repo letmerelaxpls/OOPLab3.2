c7291aed29f35e179dec47e75170fc31c767feee
Georgiy <larsubivec@gmail.com>
2022-10-04 22:12:34 +0400

    second commit

--- OOPLab3.2/Program.cs
+++ OOPLab3.2/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace OOPLab3._2
 {
+     class Book
+    {
+        public Title Title { get; set; } = new Title() { Content = Console.ReadLine() };
+        public Content Content { get; set; } = new Content() { Content = Console.ReadLine() };
+        public Author Author { get; set; } = new Author() { Content = Console.ReadLine() };
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Title.Show();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Author.Show();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Content.Show();   
+        }
+    }
+    internal class Title : BaseContent
+    {
+        public void Show() => Show("Title");
+    }
+    internal class Author : BaseContent
+    {
+        public void Show() => Show("Author");
+    }
+    internal class Content : BaseContent
+    {
+        public void Show() => Show("Content");
+
+    }
+    internal class BaseContent
+    {
+        public string Content { get; set; }
+        protected void Show(string heading) => Console.WriteLine($"{heading}: {Content}");
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Book book = new Book();
+            
+            book.Show();
+            
         }
     }
 }
